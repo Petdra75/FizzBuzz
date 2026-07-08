c48838435e4916417f63b20f9bde8850a738e08e
Petdra75 <Petru.Draghiceanu@softwire.com>
2026-07-08 12:13:13 +0300

    part2 final

--- index.ts
+++ index.ts
@@ -1,18 +1,48 @@
-function fizzBuzz(): void {
+function findBcomponent(msg : string[]): number {
+    for(let i = 0; i <= msg.length; i++){
 
+        if (msg[i].includes("B")) {
+            return i
+        }
+    }
+    return -1
+}
+
+function fizzBuzz(): void {
     for (let i : number = 1; i <=100; i++){
-        if (i % 3 === 0) {
-          console.log("Fizz");
+        const multipleOfThree : boolean = i % 3 === 0
+        const multipleOfFive : boolean = i % 5 === 0
+        const multipleOfSeven: boolean = i % 7 === 0
+        const multipleOfEleven: boolean = i % 11 === 0
+        const multipleOfThirtheen: boolean = i % 13 === 0
+        const multipleOfSeventeen: boolean = i % 17 == 0
+
+        const numberMatched : boolean = multipleOfFive || multipleOfSeven || multipleOfThree || multipleOfEleven || multipleOfThirtheen || multipleOfSeventeen
+        let message : string[] = []
+        
+        if (multipleOfThree) { message.push("Fizz")}
+        if (multipleOfFive) {message.push("Buzz")}
+        if (multipleOfSeven) (message.push("Bang"))
+         
+        if (multipleOfSeventeen){
+            message = message.reverse()
         }
-        else if(i % 5 === 0) {
-            console.log("Buzz");
+    
+        if (multipleOfEleven) {message = ["Bong"]}
+        
+        if (multipleOfThirtheen) {
+             
+            const posB : number = findBcomponent(message)
+            posB === -1 ? message.push("Fezz") : message.splice(posB, 0, "Fezz")
         }
-        else if( i % 3 === 0 && i % 5 === 0) {
-            console.log("FizzBuzz");
-        }
-        else{
-            console.log(i);
-        }
+        
+       
+        const stringMessage: string = message.join("")   
+        stringMessage !== "" ? console.log(stringMessage) : null;
+
+        if (numberMatched) {continue}
+
+        console.log(i)
     }
 }
 
